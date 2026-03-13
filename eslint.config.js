ca13cd42f3e41eed9b6a7e1ac062b1862a56e3de
Gautam Dutta <duttagautam410@gmail.com>
2026-03-13 20:02:57 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,7 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
-    rules: {
+    rules: { 
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
